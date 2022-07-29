663c4fa0b24eb76933eaf826043ebb4bf213a21b
coding4lif3 <fed.monaco@gmail.com>
2022-07-29 23:16:24 +0200

    variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+let nome;
+nome = "pippo";
+console.log(nome);
+
+
+let classe = ['pippo', 'pollo', 'gino'];
+
+
+
+console.log(nome + " pollo");
+
+console.log(classe);
+
+console.log(typeof (classe))
+console.log(typeof (nome));
+console.log(typeof (5));
+console.log(typeof (true));
+console.log(nome.length);
+
